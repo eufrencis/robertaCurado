81a2cf1bcf8b7faddef180f9266bc81ab73596a7
Francis Ferreira <francis_patricio_@hotmail.com>
2026-03-03 20:53:46 -0300

    Inserindo perguntas frequentes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 window.sr = ScrollReveal({reset:true}); /* Resetando qualquer revelaçãoa anterior */
 
 sr.reveal('.coluna-foto', {duration: 3000});
+
+document.querySelectorAll('.faq-pergunta').forEach(botao => {
+    botao.addEventListener('click', () => {
+        const item = botao.parentElement;
+        item.classList.toggle('ativo'); // Liga/desliga o efeito de abrir
+    });
+});
+
